dfe02c68eaac39726eb35ad694e2d8f4876e0a49
daviici <David@MacBook-Pro-de-David-Vanhuysse.local>
2014-03-26 17:20:29 +0000

    commit

--- sdis/src/sdis/SubscribeChannel.java
+++ sdis/src/sdis/SubscribeChannel.java
@@ -7,50 +7,55 @@ import java.net.*;
 
 public class SubscribeChannel {
 	private static MulticastSocket multiSocket;
-	private static DatagramSocket socket;
-	private static InetAddress group;
-	private static DatagramPacket sendPacket;
 	private static DatagramPacket receivePacket;
-	private static DatagramPacket multicastPacket;
 	private static byte receiveData[];
-	private static byte sendData[];
     
 	public SubscribeChannel(String strChannel, String strAdress) throws IOException {
+		receive(strChannel,strAdress);
+	}
+	private static void receive(String strChannel, String strAdress)throws IOException{
 		int port= Integer.parseInt(strChannel);
-		try {
-			socket = new DatagramSocket();
-			multiSocket = new MulticastSocket(port);
-			group = InetAddress.getByName(strAdress);
-			multiSocket.joinGroup(group);
-			sendData = new byte[65000];
-			receiveData = new byte[65000];
-			multicastPacket = new DatagramPacket(receiveData, receiveData.length);
-			multicastPacket = new DatagramPacket(sendData, sendData.length);
-			multiSocket.receive(multicastPacket);
-			multiSocket.leaveGroup(group);
-			while(true) {
-				clientRequest();
-				serverResponse();
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-    
-	
-	private static void clientRequest() throws Exception {
-		int ttl = 1;
-		sendData = new byte[65000];
-		sendPacket = new DatagramPacket(sendData, sendData.length);
-		multiSocket.send(sendPacket,(byte)ttl);
-	}
-	
-	private static void serverResponse() throws Exception {
+		// Create the socket and bind it to port 'port'.
+		multiSocket = new MulticastSocket(port);
+
+		// join the multicast group
+		multiSocket.joinGroup(InetAddress.getByName(strAdress));
+		// Now the socket is set up and we are ready to receive packets
+
+		// Create a DatagramPacket and do a receive
 		receiveData = new byte[65000];
 		receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		socket.receive(receivePacket);
+		multiSocket.receive(receivePacket);
+
+		// Finally, let us do something useful with the data we just received,
+		// like print it on stdout :-)
+		System.out.println("Received data from: " + receivePacket.getAddress().toString() +
+				    ":" + receivePacket.getPort() + " with length: " +
+				    receivePacket.getLength());
+		System.out.write(receivePacket.getData(),0,receivePacket.getLength());
+		System.out.println();
+
+		// And when we have finished receiving data leave the multicast group and
+		// close the socket
+		multiSocket.leaveGroup(InetAddress.getByName(strAdress));
+		multiSocket.close();
+
+
 	}
-    
+	private static void send(String strChannel, String strAdress, byte buf[])throws IOException{
+		int port= Integer.parseInt(strChannel);
+		// Which ttl
+		int ttl = 1;
+
+		// Create the socket but we don't bind it as we are only going to send data
+		MulticastSocket s = new MulticastSocket();
+		// Create a DatagramPacket 
+		DatagramPacket pack = new DatagramPacket(buf, buf.length,
+							 InetAddress.getByName(strAdress), port);
+		// Do a send. Note that send takes a byte for the ttl and not an int.
+		s.send(pack,(byte)ttl);
+
+		// And when we have finished sending data close the socket
+		s.close();
+	}
 }
